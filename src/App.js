a920bcabdc9311d67896914894b37cf7332a5596
mejokkurian <mejokkurian06@gmail.com>
2021-11-27 12:17:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
+// import Employee from './counter';
 
 function App() {
+  const [toDos, setToDos] = useState([])
+  console.log(toDos)
+  const [toDo, setToDo] = useState('')
+  console.log(toDo)
+
+  
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value = {toDo} onChange = {(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick = {()=>setToDos([...toDos,{id: Date.now,text:toDo,staus:false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+
+        {toDos.map((obj)=>{
+          return(
+        <div className="todo">
+          <div className="left">
+            <input onChange ={(e)=>{
+              setToDos(toDos.filter(obj2 =>{
+                if(obj2.id === obj.id){
+                  obj.staus=e.target.checked
+                }
+                return obj
+              }))
+
+            }} value={obj.staus} type="checkbox" name="" />
+            <p>{obj.text}</p>
+          </div>
+          <div className="right">
+            <i onClick ={(e)=>{
+              setToDos(toDos.filter(obj2 =>{
+                if(obj2 === obj){
+                 obj2 = e.target.null
+                }
+                return obj2
+              }))
+
+            }} className="fas fa-times" id="SDFG" ></i>
+          </div>
+        </div>)
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/counter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Employee(props) {
+   
+    // const {name,age} = props
+    // console.log(name,age)
+    // alert(props)
+    return (
+        <div>
+            <h1>counter:{props.count}</h1>
+        </div>
+    )
+}
+
+export default Employee
